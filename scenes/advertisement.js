631b80a988eda8406b6d04fec0e1612618b7c2f0
Marat Saitov <marat4568731@gmail.com>
2023-07-30 08:29:08 +0700

    advertisement

--- scenes/advertisement.js
+++ scenes/advertisement.js
@@ -3,16 +3,23 @@ const { Scenes, Markup, Composer } = require('telegraf');
 const startStep = new Composer();
 startStep.on("text", async (ctx) => {
     try {
-        ctx.wizard.state.data = {};
-        ctx.wizard.state.data.userName = ctx.message.from.username
-        ctx.wizard.state.data.firstName = ctx.message.from.first_name
-        ctx.wizard.state.data.lastName = ctx.message.from.last_name
-        await ctx.replyWithHTML("Кого Вы ищете?\n<i>Например, веб дизайнер</i>")
-        return ctx.wizard.next()
+            // Create a message with the user information
+        const message = `По вопросам размещения рекламы в боте`;
+
+        // Create an inline keyboard with the button to open the chat
+        const inlineKeyboard = Markup.inlineKeyboard([
+            Markup.button.url('Написать сообщение', 'https://t.me/makarkarim')
+        ]);
+
+        // Send the message with the inline keyboard
+        await ctx.replyWithHTML(message, inlineKeyboard);
+
+        // Proceed to the next step
+        return ctx.wizard.next();
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-})
+});
 
 const advertisementScene = new Scenes.WizardScene('advertisementWizard', startStep);
 
